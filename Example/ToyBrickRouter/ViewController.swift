a06f48987da7879b2353f9061efce8f3e6fd7edd
gloomy.meng <gloomy.meng.049@gmail.com>
2019-07-16 18:11:17 +0800

    Added: project files

---
+++ Example/ToyBrickRouter/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ToyBrickRouter
+//
+//  Created by gloomy.meng.049@gmail.com on 07/16/2019.
+//  Copyright (c) 2019 gloomy.meng.049@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
